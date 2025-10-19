ebbfac784f8c20363f722b00d1e17c192e6a369f
MthombeniKgalalelo <222294531@edu.vut.ac.za>
2025-10-19 18:25:48 +0200

    Implement Decision Tree Classifier and visualization
    Added Decision Tree Classifier for lung cancer prediction and visualized the decision tree.

--- lung_cancer_prediction.py
+++ lung_cancer_prediction.py
@@ -85,6 +85,26 @@ y_pred_log = logis.predict(X_test)
 acc_log = accuracy_score(y_test, y_pred_log)
 print("Logistic Regression Accuracy score:",acc_log * 100,"%")
 
+#   Decision Tree Classifier
+
+model = DecisionTreeClassifier(criterion="entropy", random_state=5)
+model.fit(X_train, y_train)
+y_pred_tree = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred_tree)
+print("Decision Tree Accuracy score:",accuracy * 100,"%")
+
+# Plot Decision Tree
+plt.figure(figsize=(20, 10))
+tree.plot_tree(
+    model,
+    feature_names=X.columns,
+    class_names=["NO", "YES"],
+    filled=True,
+    rounded=True
+)
+plt.title("Lung Cancer Decision Tree")
+plt.show()
+
 #   Random Forest Classifier
 
 forest = RandomForestClassifier(n_estimators=50, random_state=0)
