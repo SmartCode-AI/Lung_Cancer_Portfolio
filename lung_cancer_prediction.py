cc0ff3755b71d6b249823cb7b0101602207927fc
linamanyi <manyilina55@gmail.com>
2025-10-19 15:52:13 +0200

    Added Random Forest Classifier
    Added Random Forest Classifier for lung cancer prediction and plots.

--- lung_cancer_prediction.py
+++ lung_cancer_prediction.py
@@ -51,6 +51,24 @@ y_pred_log = logis.predict(X_test)
 acc_log = accuracy_score(y_test, y_pred_log)
 print("Logistic Regression Accuracy score:",acc_log * 100,"%")
 
+#   Random Forest Classifier
+
+forest = RandomForestClassifier(n_estimators=50, random_state=0)
+forest.fit(X_train, y_train)
+y_pred_forest = forest.predict(X_test)
+acc_forest = accuracy_score(y_test, y_pred_forest)
+print("Random Forest Accuracy score:",acc_forest * 100,"%")
+
+# Plot
+n_features = X.shape[1]
+plt.figure(figsize=(10, 6))
+plt.barh(range(n_features), forest.feature_importances_, align='center', color='red')
+plt.yticks(np.arange(n_features), X.columns)
+plt.xlabel("Feature Importance")
+plt.ylabel("Feature Name")
+plt.title("Lung Cancer Feature Importance (Random Forest)")
+plt.show(k,)
+
 #   Linear Regressor (Single Variable: AGE)
 
 X_age = X[['AGE']]
